8534579c05567476eedb213097b85aa4d7635911
Mathius <info@yogimathius.dev>
2024-03-27 22:32:57 -0700

    chore: add hardoded pong?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::Read;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,13 +13,27 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
+        println!("Incoming connection");
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                println!("+PONG\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
+
+        if bytes_read == 0 {
+            return;
+        }
+
+        stream.write_all(&buf[0..bytes_read]).expect("Failed to write to client");
     }
 }
