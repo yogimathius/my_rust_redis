48f2d306c1505be604044a86c16519b12c792cda
Mathius <info@yogimathius.dev>
2024-03-28 23:19:02 -0700

    feat: handle concurrent ping's

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(stream) => {
-                handle_client(stream);
+            Ok((stream, _)) => {
+
+                tokio::spawn(async move {
+                    handle_client(stream).await;
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -16,10 +22,10 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: TcpStream) {
     let mut buf = [0; 512];
     loop {
-        let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
+        let bytes_read = stream.read(&mut buf).await.unwrap();
 
         if bytes_read == 0 {
             return;
@@ -27,7 +33,7 @@ fn handle_client(mut stream: TcpStream) {
 
         let buf_str = std::str::from_utf8(&buf[0..bytes_read]).unwrap().to_lowercase();
         if buf_str.lines().filter(|line| { line.contains("ping")}).count() > 0 {
-            stream.write_all(b"+PONG\r\n").expect("Failed to write to client");
+            stream.write_all(b"+PONG\r\n").await.expect("Failed to write to client");
             println!("+PONG\r\n");
         } else {
             println!("Failed to extract command.");
