a296d420e1ef31c2c73b92c695c6286f2a662012
Mathius <info@yogimathius.dev>
2024-03-31 23:00:35 -0700

    fix: convert host and port to string in args

--- src/args.rs
+++ src/args.rs
@@ -1,12 +1,15 @@
+
 use anyhow::Ok;
+use crate::server::{Role, Server};
+
 mod defaults {
     pub(super) const PORT: u16 = 6379;
     pub(super) const ROLE: crate::Role = crate::Role::Master;
 }
 #[derive(Debug)]
-pub(crate) struct Args {
+pub(crate) struct Args {    
     pub(crate) port: u16,
-    pub(crate) role: crate::Role,
+    pub(crate) role: Role,
 }
 impl Args {
     // TODO: Handle errors properly
@@ -16,20 +19,6 @@ impl Args {
         
         let mut port = defaults::PORT;
         let mut role = defaults::ROLE;
-
-        // let port = match args.next().as_deref() {
-        //     Some("--port") => {
-        //         let port = args
-        //             .next()
-        //             .ok_or_else(|| anyhow::anyhow!("Expected a port number!"))?
-        //             .parse()?;
-        //         Ok(port)
-        //     }
-        //     Some("--replicaof")
-        //     Some(flag) => Err(anyhow::anyhow!("Unknown flag \"{flag}\"!")),
-        //     None => Ok(defaults::PORT),
-        // }?;
-        // Ok(Args { port })
 
         while let Some(arg) = args.next() {
             match arg.as_str() {
@@ -51,7 +40,7 @@ impl Args {
                         .next()
                         .ok_or_else(|| anyhow::anyhow!("Expected a port number!"))?
                         .parse()?;
-                    role = crate::Role::Slave { host, port };
+                    role = crate::Role::Slave { host: host.to_string(), port };
                 }
                 flag => return Err(anyhow::anyhow!("Unknown flag \"{}\"!", flag)),
             }
