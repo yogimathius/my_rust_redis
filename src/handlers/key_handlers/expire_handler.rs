e2cfed7825871047526d691ed152a560c4426302
Mathius <info@yogimathius.dev>
2024-09-14 11:34:23 -0700

    refactor: use extract_args in expire_handler

--- src/handlers/key_handlers/expire_handler.rs
+++ src/handlers/key_handlers/expire_handler.rs
@@ -1,33 +1,20 @@
-use crate::{handlers::utilities::should_set_expiry, models::value::Value, server::Server};
+use crate::{
+    handlers::utilities::{extract_args, should_set_expiry},
+    models::value::Value,
+    server::Server,
+};
 use std::time::Instant;
 
 pub fn expire_handler(server: &mut Server, args: Vec<Value>) -> Option<Value> {
     println!("expire_handler handler {:?}", args);
-    let key = match args.get(0) {
-        Some(Value::BulkString(s)) => s.clone(),
-        _ => {
-            return Some(Value::Error(
-                "ERR wrong number of arguments for 'expire' command".to_string(),
-            ))
-        }
-    };
+    let (key, expiration, option, _) = extract_args(args);
 
-    let expiration = match args.get(1) {
-        Some(Value::BulkString(s)) => s.clone(),
-        _ => {
-            return Some(Value::Error(
-                "ERR wrong number of arguments for 'expire' command".to_string(),
-            ))
-        }
-    };
-
-    let option = match args.get(2) {
-        Some(Value::BulkString(s)) => Some(s.clone()),
-        _ => None,
-    };
+    if expiration.is_none() {
+        return Some(Value::Integer(0));
+    }
 
     let mut cache = server.cache.lock().unwrap();
-    let expiration_time = expiration.parse::<i64>().unwrap();
+    let expiration_time = expiration?.parse::<i64>().unwrap();
 
     match cache.get(&key) {
         Some(value) => {
