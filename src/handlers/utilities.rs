e7556d9673621f47e4b001115a7dc5a7b8098e6d
Mathius <info@yogimathius.dev>
2024-09-14 11:33:49 -0700

    chore: create extract_args utility

--- src/handlers/utilities.rs
+++ src/handlers/utilities.rs
@@ -5,7 +5,7 @@ GT -- Set expiry only when the new expiry is greater than current one
 LT -- Set expiry only when the new expiry is less than current one
  */
 // helper function
-use crate::server::RedisItem;
+use crate::{models::value::Value, server::RedisItem};
 
 pub fn should_set_expiry(item: &RedisItem, expiration: i64, option: Option<String>) -> bool {
     println!("item {:?}", item);
@@ -33,4 +33,27 @@ pub fn should_set_expiry(item: &RedisItem, expiration: i64, option: Option<Strin
             return false;
         }
     }
+}
+
+pub fn extract_args(args: Vec<Value>) -> (String, Option<String>, Option<String>, Vec<Value>) {
+    let mut iter = args.into_iter();
+
+    let key = match iter.next() {
+        Some(Value::BulkString(s)) => s,
+        _ => "".to_string(),
+    };
+
+    let arg1 = match iter.next() {
+        Some(Value::BulkString(s)) => Some(s),
+        _ => None,
+    };
+
+    let arg2 = match iter.next() {
+        Some(Value::BulkString(s)) => Some(s),
+        _ => None,
+    };
+
+    let additional_args: Vec<Value> = iter.collect();
+
+    (key, arg1, arg2, additional_args)
 }
