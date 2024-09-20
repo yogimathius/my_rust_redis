795377882e2cedf2ec163f618a7204162edea674
Mathius <info@yogimathius.dev>
2024-09-19 22:55:16 -0700

    refactor: use lock and get item in llen handler

--- src/handlers/list_handlers/llen_handler.rs
+++ src/handlers/list_handlers/llen_handler.rs
@@ -1,12 +1,10 @@
-use std::sync::MutexGuard;
-
 use crate::{
     models::{redis_type::RedisType, value::Value},
     server::Server,
+    utilities::lock_and_get_item,
 };
 
 pub fn llen_handler(server: &mut Server, _key: String, args: Vec<Value>) -> Option<Value> {
-    // Pseudocode:
     let key = match args.get(0) {
         Some(Value::BulkString(s)) => s.clone(),
         _ => {
@@ -16,23 +14,22 @@ pub fn llen_handler(server: &mut Server, _key: String, args: Vec<Value>) -> Opti
         }
     };
 
-    let mut cache: MutexGuard<_> = server.cache.lock().unwrap();
-
-    let item = match cache.get_mut(&key) {
-        Some(item) => item,
-        None => return Some(Value::Error("ERR no such key".to_string())),
-    };
-    if let RedisType::List = item.redis_type {
-        if let Value::Array(ref list) = item.value {
-            return Some(Value::Integer(list.len() as i64));
+    match lock_and_get_item(&server.cache, &key, |item| {
+        if let RedisType::List = item.redis_type {
+            if let Value::Array(ref list) = item.value {
+                Some(Value::Integer(list.len() as i64))
+            } else {
+                Some(Value::Error(
+                    "ERR operation against a key holding the wrong kind of value".to_string(),
+                ))
+            }
         } else {
-            return Some(Value::Error(
+            Some(Value::Error(
                 "ERR operation against a key holding the wrong kind of value".to_string(),
-            ));
+            ))
         }
+    }) {
+        Ok(result) => result,
+        Err(err) => Some(err),
     }
-
-    Some(Value::Error(
-        "ERR operation against a key holding the wrong kind of value".to_string(),
-    ))
 }
