6176dc910da38ef1eb2caaf1c7b237c01252810e
Mathius <info@yogimathius.dev>
2024-09-20 23:22:16 -0700

    feat: complete rpop handler

--- checklist.md
+++ checklist.md
@@ -22,7 +22,7 @@
 - [x] `LLEN` – Get the length of a list
 - [x] `LREM` – Remove elements from a list
 - [ ] `LINDEX` – Get an element from a list by its index
-- [ ] `LPOP/RPOP` – Remove and return the first/last element from a list
+- [x] `LPOP/RPOP` – Remove and return the first/last element from a list
 - [x] `LPUSH/RPUSH` – Prepend/Append an element to a list
 - [x] `LSET` – Set the value of an element in a list by its index
 
--- tests/list_rpop_handler_test.rs
+++ tests/list_rpop_handler_test.rs
@@ -2,10 +2,13 @@
 mod tests {
     use std::collections::HashMap;
     use std::sync::{Arc, Mutex};
+    use std::time::Instant;
 
-    use my_redis_server::handlers::{lset_handler, rpop_handler};
+    use my_redis_server::handlers::{rpop_handler, rpush_handler};
+    use my_redis_server::models::redis_type::RedisType;
     use my_redis_server::models::value::Value;
-    use my_redis_server::server::{Role, Server};
+    use my_redis_server::server::{RedisItem, Role, Server};
+
     fn setup() -> Server {
         Server {
             cache: Arc::new(Mutex::new(HashMap::new())),
@@ -16,18 +19,26 @@ mod tests {
     }
 
     #[test]
-    fn test_rpop_handler() {
+    fn test_rpop_handler_existing_list() {
         let mut server = setup();
-        let args = vec![
-            Value::BulkString("key".to_string()),
-            Value::BulkString("value".to_string()),
+        let key = "key".to_string();
+        let initial_list = vec![
+            Value::BulkString("initial".to_string()),
+            Value::BulkString("second".to_string()),
         ];
-        lset_handler(&mut server, "key".to_string(),  args);
-        let args = vec![
-            Value::BulkString("key".to_string()),
-            Value::BulkString("10".to_string()),
-        ];
-        let result = rpop_handler(&mut server, "key".to_string(),  args);
-        assert_eq!(result, Some(Value::SimpleString("OK".to_string())));
+
+        let redis_item = RedisItem {
+            value: Value::Array(initial_list),
+            expiration: None,
+            created_at: Instant::now(),
+            redis_type: RedisType::List,
+        };
+
+        server.cache.lock().unwrap().insert(key.clone(), redis_item);
+
+        let args = vec![];
+
+        let result = rpop_handler(&mut server, key.clone(), args);
+        assert_eq!(result, Some(Value::BulkString("second".to_string())));
     }
 }
