b11173265c240ca988e23ff6eb9b0c75840e2c91
Mathius <info@yogimathius.dev>
2024-09-20 23:17:50 -0700

    feat: complete lpop handler

--- src/handlers/list_handlers/lpop_handler.rs
+++ src/handlers/list_handlers/lpop_handler.rs
@@ -1,10 +1,21 @@
 use crate::{models::value::Value, server::Server};
 
-pub fn lpop_handler(_server: &mut Server, _key: String, _args: Vec<Value>) -> Option<Value> {
-    // Pseudocode:
-    // 1. Extract the key from args.
-    // 2. Lock the cache.
-    // 3. Retrieve the list associated with the key.
-    // 4. Remove and return the first element of the list as a BulkString.
-    Some(Value::SimpleString("OK".to_string()))
+pub fn lpop_handler(server: &mut Server, key: String, _args: Vec<Value>) -> Option<Value> {
+    let mut cache = server.cache.lock().unwrap();
+    match cache.get_mut(&key) {
+        Some(item) => {
+            if let Value::Array(ref mut list) = item.value {
+                if list.is_empty() {
+                    Some(Value::NullBulkString)
+                } else {
+                    Some(list.remove(0))
+                }
+            } else {
+                Some(Value::Error(
+                    "ERR operation against a key holding the wrong kind of value".to_string(),
+                ))
+            }
+        }
+        None => Some(Value::NullBulkString),
+    }
 }
--- tests/list_lpop_handler_test.rs
+++ tests/list_lpop_handler_test.rs
@@ -3,9 +3,10 @@ mod tests {
     use std::collections::HashMap;
     use std::sync::{Arc, Mutex};
 
-    use my_redis_server::handlers::{lpop_handler, lset_handler};
+    use my_redis_server::handlers::{lpop_handler, rpush_handler};
     use my_redis_server::models::value::Value;
     use my_redis_server::server::{Role, Server};
+
     fn setup() -> Server {
         Server {
             cache: Arc::new(Mutex::new(HashMap::new())),
@@ -16,18 +17,16 @@ mod tests {
     }
 
     #[test]
-    fn test_lpop_handler() {
+    fn test_lpop_handler_existing_list() {
         let mut server = setup();
-        let args = vec![
-            Value::BulkString("key".to_string()),
-            Value::BulkString("value".to_string()),
-        ];
-        lset_handler(&mut server, "key".to_string(),  args);
-        let args = vec![
-            Value::BulkString("key".to_string()),
-            Value::BulkString("10".to_string()),
-        ];
-        let result = lpop_handler(&mut server, "key".to_string(),  args);
-        assert_eq!(result, Some(Value::SimpleString("OK".to_string())));
+        let key = "key".to_string();
+        let initial_list = vec![Value::BulkString("initial".to_string())];
+
+        rpush_handler(&mut server, key.clone(), initial_list);
+
+        let args = vec![];
+
+        let result = lpop_handler(&mut server, key.clone(), args);
+        assert_eq!(result, Some(Value::BulkString("initial".to_string())));
     }
 }
