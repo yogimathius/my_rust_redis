7599962db84d781074b3c5b646189d7cb0011236
Mathius <info@yogimathius.dev>
2024-10-07 21:53:30 -0700

    chore: small cleanup in resp (add soon)

--- src/resp.rs
+++ src/resp.rs
@@ -12,7 +12,7 @@ use crate::commands::COMMAND_HANDLERS;
 use crate::log;
 use crate::models::value::Value;
 use crate::server::Server;
-use crate::utilities::{extract_command, parse_message};
+use crate::utilities::{extract_command, parse_message, unpack_bulk_str};
 
 pub struct RespHandler {
     stream: TcpStream,
@@ -29,20 +29,13 @@ impl RespHandler {
         }
     }
 
-    pub async fn handle_client(
-        &mut self,
-        server: Arc<Mutex<Server>>,
-        sender: Arc<broadcast::Sender<Value>>,
-    ) -> Result<()> {
-        let sender = Arc::clone(&sender);
-
+    pub async fn handle_client(&mut self, server: Arc<Mutex<Server>>) -> Result<()> {
         loop {
             let value: Option<Value> = self.read_value().await?;
 
             let response: Option<Value> = if let Some(value) = value {
                 log!("value: {:?}", value);
-                self.execute_command(value, server.clone(), sender.clone())
-                    .await
+                self.execute_command(value, server.clone()).await
             } else {
                 None
             };
@@ -84,27 +77,18 @@ impl RespHandler {
         &mut self,
         value: Value,
         server: Arc<Mutex<Server>>,
-        sender: Arc<broadcast::Sender<Value>>,
     ) -> Option<Value> {
-        let (command, key, args) = extract_command(value.clone()).unwrap();
+        let (command, args) = extract_command(&value.clone()).unwrap();
+        let key = unpack_bulk_str(args.get(0).unwrap()).unwrap();
         if command == "SET" {
             println!("SET command");
-            let response = sender.send(value);
-            log!("response: {:?}", response);
         }
-        if command == "PSYNC" {
-            let mut receiver = sender.subscribe();
-
-            while let Ok(f) = receiver.recv().await {
-                self.stream
-                    .write_all(&f.serialize().as_bytes())
-                    .await
-                    .unwrap();
-            }
-        }
+        if command == "PSYNC" {}
         log!("command: {}", command);
         if let Some(command_function) = COMMAND_HANDLERS.get(command.as_str()) {
-            command_function.handle(server.clone(), key, args).await
+            command_function
+                .handle(server.clone().lock().await.data.clone(), key, args)
+                .await
         } else {
             Value::SimpleString("Unknown command".to_string());
             std::process::exit(1);
