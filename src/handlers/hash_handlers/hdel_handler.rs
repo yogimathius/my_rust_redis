a42a42453e388d263da6e1c4ca759cd676f006e5
Mathius <info@yogimathius.dev>
2024-10-12 11:56:00 -0700

    feat: refactor hdel with new operate on hash fn

--- src/handlers/hash_handlers/hdel_handler.rs
+++ src/handlers/hash_handlers/hdel_handler.rs
@@ -1,41 +1,23 @@
-use crate::{
-    models::{redis_type::RedisType, value::Value},
-    server::Server,
-};
+use super::hash_utils::HashOperation;
+use crate::{models::value::Value, server::Server};
 
 pub fn hdel_handler(server: &mut Server, key: String, args: Vec<Value>) -> Option<Value> {
     if args.is_empty() {
         return Some(Value::Integer(0));
     }
-
-    let mut cache = server.cache.lock().unwrap();
-    match cache.get_mut(&key) {
-        Some(item) => {
-            if let RedisType::Hash = item.redis_type {
-                let mut count = 0;
-                if let Value::Hash(ref mut hash) = item.value {
-                    for field in args {
-                        if let Value::BulkString(field) = field {
-                            if hash.remove(&field).is_some() {
-                                count += 1;
-                            }
-                        } else {
-                            return Some(Value::Error(
-                                "ERR arguments must contain a value for every field".to_string(),
-                            ));
-                        }
-                    }
-                    return Some(Value::Integer(count));
+    server.operate_on_hash(&key, |hash| {
+        let mut count = 0;
+        for field in args {
+            if let Value::BulkString(field) = field {
+                if hash.remove(&field).is_some() {
+                    count += 1;
                 }
-                Some(Value::Error(
-                    "ERR operation against a key holding the wrong kind of value".to_string(),
-                ))
             } else {
-                Some(Value::Error(
-                    "ERR operation against a key holding the wrong kind of value".to_string(),
-                ))
+                return Some(Value::Error(
+                    "ERR arguments must contain a value for every field".to_string(),
+                ));
             }
         }
-        None => Some(Value::Integer(0)),
-    }
+        Some(Value::Integer(count))
+    })
 }
