6ff4e3a0c9eabfbfaacdd1c7743b650aabbde380
Mathius <info@yogimathius.dev>
2024-10-12 11:10:50 -0700

    feat: add new methods to redis_item

--- src/models/redis_item.rs
+++ src/models/redis_item.rs
@@ -1,6 +1,8 @@
+use crate::models::redis_type::RedisType;
+use crate::models::value::Value;
 use serde::{Deserialize, Serialize};
-
-use super::{redis_type::RedisType, value::Value};
+use std::collections::HashMap;
+use std::time::SystemTime;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RedisItem {
@@ -8,4 +10,68 @@ pub struct RedisItem {
     pub created_at: i64,
     pub expiration: Option<i64>,
     pub redis_type: RedisType,
+}
+
+impl RedisItem {
+    pub fn new_hash(hash: HashMap<String, Value>) -> Self {
+        RedisItem {
+            value: Value::Hash(hash),
+            created_at: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_secs() as i64,
+            expiration: None,
+            redis_type: RedisType::Hash,
+        }
+    }
+
+    pub fn new_string(s: String) -> Self {
+        RedisItem {
+            value: Value::BulkString(s),
+            created_at: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_secs() as i64,
+            expiration: None,
+            redis_type: RedisType::String,
+        }
+    }
+
+    pub fn new_list(list: Vec<Value>) -> Self {
+        RedisItem {
+            value: Value::Array(list),
+            created_at: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_secs() as i64,
+            expiration: None,
+            redis_type: RedisType::List,
+        }
+    }
+
+    // A general constructor that can be used for any type
+    pub fn new(value: Value, redis_type: RedisType) -> Self {
+        RedisItem {
+            value,
+            created_at: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_secs() as i64,
+            expiration: None,
+            redis_type,
+        }
+    }
+
+    pub fn is_expired(&self) -> bool {
+        self.expiration
+            .map(|duration| {
+                SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .expect("Time went backwards")
+                    .as_secs() as i64
+                    - self.created_at
+                    >= duration
+            })
+            .unwrap_or(false)
+    }
 }
