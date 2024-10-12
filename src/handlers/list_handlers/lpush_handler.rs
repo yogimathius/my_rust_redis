c48bb77d5fc6e460dcb47ed652d044bf4b5c99e2
Mathius <info@yogimathius.dev>
2024-10-12 00:16:48 -0700

    fix: better handle lpush logic with logs and error messaging

--- src/handlers/list_handlers/lpush_handler.rs
+++ src/handlers/list_handlers/lpush_handler.rs
@@ -1,39 +1,38 @@
-use std::time::Instant;
-
-use crate::{
-    models::{redis_item::RedisItem, redis_type::RedisType, value::Value},
-    server::Server,
-};
+use crate::{log, models::value::Value, server::Server, utilities::lock_and_get_item};
 
 pub fn lpush_handler(server: &mut Server, key: String, args: Vec<Value>) -> Option<Value> {
-    let new_item = match args.get(0) {
-        Some(Value::BulkString(v)) => v.clone(),
-        _ => return Some(Value::Error("ERR value is not a bulk string".to_string())),
-    };
+    log!("LPUSH: Handling key '{}' with args: {:?}", key, args);
 
-    let mut cache = server.cache.lock().unwrap();
-    match cache.get_mut(&key) {
-        Some(item) => {
-            if let Value::Array(ref mut list) = item.value {
-                list.insert(0, Value::BulkString(new_item));
-                Some(Value::Integer(list.len() as i64))
-            } else {
-                Some(Value::Error(
-                    "ERR operation against a key holding the wrong kind of value".to_string(),
-                ))
+    match lock_and_get_item(&server.cache, &key, |item| match &mut item.value {
+        Value::Array(list) => {
+            for arg in args.iter().rev() {
+                list.insert(0, arg.clone());
             }
+            log!(
+                "LPUSH: Updated existing list for key '{}'. New length: {}",
+                key,
+                list.len()
+            );
+            Some(Value::Integer(list.len() as i64))
         }
-        None => {
-            let list = vec![Value::BulkString(new_item)];
-            let redis_item = RedisItem {
-                value: Value::Array(list),
-                expiration: None,
-                created_at: Instant::now().elapsed().as_secs() as i64,
-                redis_type: RedisType::List,
-            };
-
-            cache.insert(key.clone(), redis_item);
-            Some(Value::Integer(1))
+        _ => {
+            let new_list: Vec<Value> = args.into_iter().rev().collect();
+            item.value = Value::Array(new_list.clone());
+            log!(
+                "LPUSH: Created new list for key '{}'. Length: {}",
+                key,
+                new_list.len()
+            );
+            Some(Value::Integer(new_list.len() as i64))
+        }
+    }) {
+        Ok(result) => {
+            log!("LPUSH: Operation successful for key '{}'", key);
+            result
+        }
+        Err(err) => {
+            log!("LPUSH: Error for key '{}': {:?}", key, err);
+            Some(err)
         }
     }
 }
