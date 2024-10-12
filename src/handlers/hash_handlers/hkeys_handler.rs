39d67ffaa419e348c3968c5bb66dd6cb36c37f2d
Mathius <info@yogimathius.dev>
2024-10-12 12:26:59 -0700

    refactor: update hkeys with new operate fn

--- src/handlers/hash_handlers/hkeys_handler.rs
+++ src/handlers/hash_handlers/hkeys_handler.rs
@@ -1,31 +1,11 @@
-use crate::{
-    log,
-    models::{redis_type::RedisType, value::Value},
-    server::Server,
-};
+use super::hash_utils::HashOperation;
+use crate::{models::value::Value, server::Server};
 
 pub fn hkeys_handler(server: &mut Server, key: String, _: Vec<Value>) -> Option<Value> {
-    let mut cache = server.cache.lock().unwrap();
-    match cache.get_mut(&key) {
-        Some(item) => {
-            log!("Item: {:?}", item);
-            if let RedisType::Hash = item.redis_type {
-                if let Value::Hash(hash) = &item.value {
-                    let mut keys: Vec<_> = hash.keys().cloned().collect();
-                    keys.sort();
-                    let keys = keys.into_iter().map(Value::BulkString).collect();
-                    Some(Value::Array(keys))
-                } else {
-                    Some(Value::Error(
-                        "ERR operation against a key holding the wrong kind of value".to_string(),
-                    ))
-                }
-            } else {
-                Some(Value::Error(
-                    "ERR operation against a key holding the wrong kind of value".to_string(),
-                ))
-            }
-        }
-        None => Some(Value::Array(vec![])),
-    }
+    server.operate_on_hash(&key, |hash| {
+        let mut keys: Vec<_> = hash.keys().cloned().collect();
+        keys.sort();
+        let keys = keys.into_iter().map(Value::BulkString).collect();
+        Some(Value::Array(keys))
+    })
 }
