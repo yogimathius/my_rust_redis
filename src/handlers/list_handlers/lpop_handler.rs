c9d708e6bdbd361be0fa4aecfe4e32b92315c0a1
Mathius <info@yogimathius.dev>
2024-10-12 21:27:05 -0700

    refactor: use operate on list in lpop handler

--- src/handlers/list_handlers/lpop_handler.rs
+++ src/handlers/list_handlers/lpop_handler.rs
@@ -1,21 +1,16 @@
+use super::list_utils::ListOperation;
 use crate::{models::value::Value, server::Server};
 
 pub fn lpop_handler(server: &mut Server, key: String, _args: Vec<Value>) -> Option<Value> {
-    let mut cache = server.cache.lock().unwrap();
-    match cache.get_mut(&key) {
-        Some(item) => {
-            if let Value::Array(ref mut list) = item.value {
-                if list.is_empty() {
-                    Some(Value::NullBulkString)
-                } else {
-                    Some(list.remove(0))
-                }
+    server
+        .operate_on_list(&key, |list| {
+            if list.is_empty() {
+                Some(Value::NullBulkString)
             } else {
-                Some(Value::Error(
-                    "ERR operation against a key holding the wrong kind of value".to_string(),
-                ))
+                Some(list.remove(0))
             }
-        }
-        None => Some(Value::NullBulkString),
-    }
+        })
+        .or(Some(Value::Error(
+            "ERR operation against a key holding the wrong kind of value".to_string(),
+        )))
 }
